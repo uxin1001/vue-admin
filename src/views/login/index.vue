cf8b08e57c73cd1af6558856cabb569dd714c1cd
uxin1001 <1329220172@qq.com>
2020-07-02 23:28:58 +0800

    登录页面修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,8 +2,40 @@
   <div id="login">
     <div class="login-wrap">
       <ul class="menu-tab">
-        <li class=("current":isActive) v-for="item in menuTab" :key="item.id">{{ item.text }}</li>
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{'current' : item.current}"
+          @click="toggleMenn(item)"
+        >{{ item.text }}</li>
       </ul>
+      <!-- 表单 start -->
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        class="login-from"
+        size="medium"
+      >
+        <el-form-item prop="pass" class="item-from">
+          <label>邮箱</label>
+          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="checkPass" class="item-from">
+          <label>密码</label>
+          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="age" class="item-from">
+          <label>验证码</label>
+          <el-input v-model.number="ruleForm.age"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -11,16 +43,83 @@
 <script>
 export default {
   name: "login",
+
   data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
-      menuTab: [{ text: "登录" }, { text: "注册" }],
-      isActive:true
+      menuTab: [
+        { text: "登录", current: true },
+        { text: "注册", current: false }
+      ],
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        age: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }]
+      }
     };
   },
+
   created() {},
+  //挂载完成后自动执行的
   mounted() {},
+  //写函数的地方
   methods: {
-    //vue 数据驱动视频渲染
+    toggleMenn(data) {
+      this.menuTab.forEach((elem, index) => {
+        elem.current = false;
+      });
+      //高光
+      data.current = true;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
   }
 };
 </script>
@@ -48,5 +147,20 @@ export default {
 }
 .current {
   background-color: rgba(0, 0, 0, 0.1);
+}
+.login-from {
+  margin-top: 28px;
+  width: 400px;
+  margin: auto;
+
+  label {
+    display: block;
+    font-size: 14px;
+    margin-bottom: 4px;
+    color:#fff;
+  }
+  .item-from {
+  margin-bottom: 13px;
+  }
 }
 </style>
